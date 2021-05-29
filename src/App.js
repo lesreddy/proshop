42fa4b92a4dd55a5c5acb4deaee189c710db7222
Les Reddy <les.reddy@gmail.com>
2021-05-29 15:08:25 +1000

    homescreen product listing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { Container } from 'react-bootstrap'
+import { Container } from "react-bootstrap";
 
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <main className="py-3">
-        <Container>
-        <h1>Welcome</h1>
-        </Container>
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    <HomeScreen />
+                </Container>
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
