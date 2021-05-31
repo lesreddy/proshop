24279ed01a20f9ff00deeeba2a08d356180f508a
Les Reddy <les.reddy@gmail.com>
2021-05-31 22:20:45 +1000

    Implementing React Router

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
     return (
         <footer>
             <Container>
                 <Row>
-                    <Col className="text-center py-3">Copyright &copy; Proshop</Col>
+                    <Col className="text-center py-3">
+                        Copyright &copy; Proshop
+                    </Col>
                 </Row>
             </Container>
         </footer>
-    )
+    );
 }
 
-export default Footer
+export default Footer;
